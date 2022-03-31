e7460b1b5de49e47344750562c76af6f6321a69f
zaidamorales <60615803+zaidamorales@users.noreply.github.com>
2022-03-31 11:54:24 +0200

    Add files via upload

---
+++ src/com/company/Chair.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Chair {
+
+    float height, width;
+
+    public float getWidth() {
+        return this.width;
+    }
+    public void setWidth(float width) {
+        this.width = width;
+    }
+
+    public float getHeight() {
+        return this.height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+}
+
+}
---
+++ src/com/company/Square.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class Square {
+
+    float height, width, area;
+
+    public float getWidth() {
+        return this.width;
+    }
+    public void setWidth(float width) {
+        this.width = width;
+    }
+
+    public float getArea() {
+        return this.area;
+    }
+
+    public void setArea(float area) {
+        this.area = area;
+    }
+
+    public float getHeight() {
+        return this.height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+}
